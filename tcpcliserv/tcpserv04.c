d0116f588dd74a413cbc46c2c99960e48f03e52b
wangjj <wangjj@qtec.cn>
2018-05-08 04:15:43 -0700

    在网络编程时，可能会遇到的3种情况： 1.当fork 子进程时，必须捕获SIGCHLD信号； 2.当捕获信号时，必须处理被中断的系统调用； 3.SIGCHLD 的信号处理函数必须正确编写，应使用waitpid函数以免留下僵尸进程

---
+++ tcpcliserv/tcpserv04.c
@@ -0,0 +1,42 @@
+#include "unp.h"
+
+int main(int argc, char ** argv)
+{
+    int listenfd,connfd;
+    pid_t childpid;
+    socklen_t clilen;
+    struct sockaddr_in cliaddr,servaddr;
+    void sig_chld(int);
+
+    listenfd=socket(AF_INET,SOCK_STREAM,0);
+
+    bzero(&servaddr,sizeof(servaddr));
+    servaddr.sin_family=AF_INET;
+    servaddr.sin_addr.s_addr=htonl(INADDR_ANY);
+    servaddr.sin_port=htons(SERV_PORT);
+    bind(listenfd,(SA *)&servaddr,sizeof(servaddr));
+    listen(listenfd,LISTENQ);
+    signal(SIGCHLD,sig_chld); /*must call waitpid()*/
+    for(;;)
+    {
+        clilen=sizeof(cliaddr);
+        if((connfd=accept(listenfd,(SA *)&cliaddr, &clilen))<0)
+        {
+            if(errno==EINTR)
+                continue; /*back to for()*/
+            else 
+            {
+                printf("accept error\n");
+                return -1;
+            }
+        }
+        if( (childpid=fork())==0)
+        {
+            //child process
+            close(listenfd);
+            str_echo(connfd);
+            exit(0);
+        }
+        close(connfd); //parent closes connected socket
+    }
+}
