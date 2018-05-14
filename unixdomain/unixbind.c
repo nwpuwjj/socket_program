57fbb9ee54cf6dec6ad957afeb8e048cf5db50cc
wangjj <wangjj@qtec.cn>
2018-05-14 01:54:05 -0700

    unix 域

---
+++ unixdomain/unixbind.c
@@ -0,0 +1,29 @@
+#include "unp.h"
+
+int main(int argc, char **argv)
+{
+    int sockfd;
+    socklen_t len;
+    struct sockaddr_un addr1, addr2;
+
+    if(argc!=2)
+    {
+        printf("usage: unixbind <pathname>\n");
+        return -1;
+    }
+
+    sockfd = socket(AF_LOCAL,SOCK_STREAM,0);
+
+    unlink(argv[1]); /*ok if this fails*/
+
+    bzero(&addr1,sizeof(addr1));
+    addr1.sun_family=AF_LOCAL;
+    strncpy(addr1.sun_path,argv[1],sizeof(addr1.sun_path)-1);
+    bind(sockfd,(SA *)&addr1, SUN_LEN(&addr1));
+    
+    len=sizeof(addr2);
+    getsockname(sockfd,(SA *)&addr2,&len);
+    printf("bound name = %s, reutrn len = %d\n", addr2.sun_path, len);
+
+    exit(0);
+}
